f47e06285903838937455841e6ddc7825ca00ed6
Cora <silber_alge@web.de>
2025-03-12 12:01:45 +0100

    Added style.css Added overview.py to see different texts Added Icon

--- frontend/main.py
+++ frontend/main.py
@@ -1,1 +1,62 @@
-#this is a test
+import PySide6.QtCore
+import sys
+import os
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QPushButton, QMainWindow
+
+
+class MoonDrift(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Moondrift")
+        self.setGeometry(0, 0, 612, 1017)
+        app_icon = QtGui.QIcon(os.path.abspath('C:\Moondrift/assets/Logo.ico'))  # Hier den Pfad zu deinem Icon angeben
+        self.setWindowIcon(app_icon)
+        self.setStyleSheet("background-color: #092139;")
+        layout = QtWidgets.QVBoxLayout()
+
+        # Setze den Abstand zwischen den Widgets auf einen kleinen Wert (z.B. 5)
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        # Header
+        self.header = QtWidgets.QLabel("Hello Sleepy(˶ᵔ ᵕ ᵔ˶)", alignment=QtCore.Qt.AlignCenter)
+        self.header.setObjectName("moondrift_header")
+        self.header.setStyleSheet("padding: 0; margin: 5;")
+        layout.addWidget(self.header)
+
+        # Welcome
+        self.welcome = QtWidgets.QLabel("Welcome to Moondrift૮₍ ˃ ⤙ ˂ ₎ა", alignment=QtCore.Qt.AlignCenter)
+        self.welcome.setObjectName("moondrift_h1")
+        self.welcome.setStyleSheet("padding: 0; margin: 5;")
+        layout.addWidget(self.welcome)
+
+        # Info
+        self.info = QtWidgets.QLabel(
+            "Sadly there is not much here yet, check out the read me for more info( •̯́ ₃ •̯̀) ",
+            alignment=QtCore.Qt.AlignCenter)
+        self.info.setObjectName("settings_text")
+        self.welcome.setStyleSheet("padding: 0; margin: 5;")
+        layout.addWidget(self.info)
+
+        layout.addStretch(1)
+
+        self.setLayout(layout)
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    # Stylesheet aus einer Datei laden
+    with open("style.css", "r", encoding="utf-8") as f:
+        stylesheet = f.read()
+        app.setStyleSheet(stylesheet)  # Das Stylesheet anwenden
+
+    window = MoonDrift()
+    window.show()
+    sys.exit(app.exec())
---
+++ frontend/overview.py
@@ -0,0 +1,96 @@
+import sys
+import os
+from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class text_overview(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Moondrift")
+        self.setGeometry(0, 0, 412, 917)
+        self.setStyleSheet("background-color: #092139;")
+        app_icon = QtGui.QIcon(os.path.abspath('C:\Moondrift/assets/Logo.ico'))
+        self.setWindowIcon(app_icon)
+        layout = QtWidgets.QVBoxLayout()
+
+        # Header
+        self.header = QtWidgets.QLabel("This is the Header", alignment=QtCore.Qt.AlignLeft)
+        self.header.setObjectName("moondrift_header")
+        layout.addWidget(self.header)
+
+        # H1
+        self.h1 = QtWidgets.QLabel("This is h1", alignment=QtCore.Qt.AlignLeft)
+        self.h1.setObjectName("moondrift_h1")
+        layout.addWidget(self.h1)
+
+        # Home Button
+        self.home_button = QtWidgets.QLabel("This is Home Button Text", alignment=QtCore.Qt.AlignLeft)
+        self.home_button.setObjectName("home_buttons")
+        layout.addWidget(self.home_button)
+
+        # Stats Diagram Text Stats
+        self.diagram_stats = QtWidgets.QLabel("This is Stats Diagram", alignment=QtCore.Qt.AlignLeft)
+        self.diagram_stats.setObjectName("stats_diagram_text_stats")
+        layout.addWidget(self.diagram_stats)
+
+        # Stats Diagram Text Date
+        self.diagram_date = QtWidgets.QLabel("This is Stats Diagram", alignment=QtCore.Qt.AlignLeft)
+        self.diagram_date.setObjectName("stats_diagram_text_date")
+        layout.addWidget(self.diagram_date)
+
+        # Explore Sound Text
+        self.sound_text = QtWidgets.QLabel("This is Explore Sound Text", alignment=QtCore.Qt.AlignLeft)
+        self.sound_text.setObjectName("explore_sound_text")
+        layout.addWidget(self.sound_text)
+
+        # Explore Artist Text
+        self.artist_text = QtWidgets.QLabel("This is Explore Artist Text", alignment=QtCore.Qt.AlignLeft)
+        self.artist_text.setObjectName("explore_artist_text")
+        layout.addWidget(self.artist_text)
+
+        # Profile Text Description
+        self.description_text = QtWidgets.QLabel("This is Profile Text Description", alignment=QtCore.Qt.AlignLeft)
+        self.description_text.setObjectName("profile_text_description")
+        layout.addWidget(self.description_text)
+
+        # Settings Header
+        self.settings_header = QtWidgets.QLabel("This is Settings Header", alignment=QtCore.Qt.AlignLeft)
+        self.settings_header.setObjectName("settings_header")
+        layout.addWidget(self.settings_header)
+
+        # Settings h1
+        self.settings_h1 = QtWidgets.QLabel("This is settings_h1", alignment=QtCore.Qt.AlignLeft)
+        self.settings_h1.setObjectName("settings_h1")
+        layout.addWidget(self.settings_h1)
+
+        # Settings text
+        self.settings_text = QtWidgets.QLabel("This is settings_text", alignment=QtCore.Qt.AlignLeft)
+        self.settings_text.setObjectName("settings_text")
+        layout.addWidget(self.settings_text)
+
+        # settings_fillout_text
+        self.settings_fillout_text = QtWidgets.QLabel("This is settings_fillout_text", alignment=QtCore.Qt.AlignLeft)
+        self.settings_fillout_text.setObjectName("settings_fillout_text")
+        layout.addWidget(self.settings_fillout_text)
+
+        # settings_button_text
+        self.settings_button_text = QtWidgets.QLabel("This is settings_button_text", alignment=QtCore.Qt.AlignLeft)
+        self.settings_button_text.setObjectName("settings_button_text")
+        layout.addWidget(self.settings_button_text)
+
+        self.setLayout(layout)
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    # Stylesheet aus einer Datei laden
+    with open("style.css", "r", encoding="utf-8") as f:
+        stylesheet = f.read()
+        app.setStyleSheet(stylesheet)  # Das Stylesheet anwenden
+
+    window = text_overview()
+    window.show()
+    sys.exit(app.exec())
