caee3a79820de9c81216a9042fd869c1d280ad02
Cora <silber_alge@web.de>
2025-03-14 08:57:54 +0100

    Added Gifs for Loading Screen Changed window, so it cant be resized

--- frontend/main.py
+++ frontend/main.py
@@ -3,7 +3,7 @@ import sys
 import os
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtGui import QGuiApplication
+from PySide6.QtGui import QGuiApplication, QMovie
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QPushButton, QMainWindow
 
@@ -14,10 +14,13 @@ class MoonDrift(QtWidgets.QWidget):
 
         self.setWindowTitle("Moondrift")
         self.setGeometry(0, 0, 612, 1017)
-        app_icon = QtGui.QIcon(os.path.abspath('C:\Moondrift/assets/Logo.ico'))  # Hier den Pfad zu deinem Icon angeben
+        app_icon = QtGui.QIcon(os.path.abspath('C:/Moondrift/assets/Logo.ico'))
         self.setWindowIcon(app_icon)
         self.setStyleSheet("background-color: #092139;")
         layout = QtWidgets.QVBoxLayout()
+
+        # Disable fullscreen and resizing
+        self.setFixedSize(612, 1017)
 
         # Setze den Abstand zwischen den Widgets auf einen kleinen Wert (z.B. 5)
         layout.setSpacing(0)
@@ -40,16 +43,41 @@ class MoonDrift(QtWidgets.QWidget):
             "Sadly there is not much here yet, check out the read me for more info( •̯́ ₃ •̯̀) ",
             alignment=QtCore.Qt.AlignCenter)
         self.info.setObjectName("settings_text")
-        self.welcome.setStyleSheet("padding: 0; margin: 5;")
+        self.info.setStyleSheet("padding: 0; margin: 5;")
         layout.addWidget(self.info)
+
+        ##### GIF ####
+        self.giflabel = QtWidgets.QLabel(self)
+        self.giflabel.setMinimumSize(QtCore.QSize(500, 500))
+        self.giflabel.setMaximumSize(QtCore.QSize(1000, 1000))
+        self.giflabel.setAlignment(QtCore.Qt.AlignCenter)
+
+        # Integrate QMovie to the label and initiate the GIF
+        self.gif = QMovie('C:/Moondrift/assets/test.gif')
+        self.giflabel.setMovie(self.gif)
+        self.gif.start()
+        # Add the GIF label to the layout
+        layout.addWidget(self.giflabel)
 
         layout.addStretch(1)
 
+        ###Loading Gif ###
+        self.loadinggiflabel = QtWidgets.QLabel(self)
+        self.loadinggiflabel.setMinimumSize(QtCore.QSize(200, 200))
+        self.loadinggiflabel.setMaximumSize(QtCore.QSize(1000, 1000))
+        self.loadinggiflabel.setAlignment(QtCore.Qt.AlignCenter)
+
+        # Integrate QMovie to the label and initiate the GIF
+        self.loadinggif = QMovie('C:/Moondrift/assets/Eclipse@1x-0.7s-200px-200px (1).gif')
+        self.loadinggiflabel.setMovie(self.loadinggif)
+        self.loadinggif.start()
+        # Add the GIF label to the layout
+        layout.addWidget(self.loadinggiflabel)
+
         self.setLayout(layout)
 
-
-
 if __name__ == "__main__":
+    import sys
     app = QtWidgets.QApplication(sys.argv)
 
     # Stylesheet aus einer Datei laden
